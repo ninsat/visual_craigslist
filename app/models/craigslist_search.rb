02f84b56152a11f9ec23bd46bf705f31d62bf11f
Mischa <f.mischa@gmail.com>
2011-06-18 18:16:53 -0500

    initial

---
+++ app/models/craigslist_search.rb
@@ -0,0 +1,25 @@
+class CraigslistSearch
+
+  # srchType
+  # minAsk
+  # maxAsk
+  # bedrooms
+  # hasPic
+
+
+   # http://boston.craigslist.org/search/sub/gbs?query=&srchType=A&minAsk=400&maxAsk=1500&bedrooms=&hasPic=1
+
+   def self.query
+
+   end
+
+end
+
+
+CraigslistSearch.query(:text => 'asdf', :min => 300, :max => 400)
+
+http://boston.craigslist.org/search/sub/gbs?query=davis+square&srchType=A&minAsk=400&maxAsk=1500&bedrooms=&hasPic=1
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apartments::Application.config.secret_token = '4278aaa54614b394b6f4a42bac307c39b7794b18d52a431ab03d9bcc5ca5da0c63dc9aeac61f338dead02087b9de508c06e3f3ab445912d3980c542c95d31946'
